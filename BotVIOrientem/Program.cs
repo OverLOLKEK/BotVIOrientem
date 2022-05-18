e07aa7f4cb2785f446ffd34389c37e3bdab18c3c
Владислав Кузнецов <80295591+OverLOLKEK@users.noreply.github.com>
2022-05-18 10:36:39 +1000

    add

--- BotVIOrientem/Program.cs
+++ BotVIOrientem/Program.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace VIOrientemBot
 {
@@ -17,41 +18,86 @@ namespace VIOrientemBot
         public long id;
         public string? username;
     }
+    class Person
+    {
+        public string Name { get; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
     internal class Program
     {
         static ITelegramBotClient bot = new TelegramBotClient("5383320170:AAG-6lKyg1tTJvYNvn9oYC_jnoUKYIszpKQ");       
         static string fileName = "updates.json";
+        static bool logging = false;
+      
         static List<BotUpdate> botUpdates = new List<BotUpdate>();       
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            
-            // Некоторые действия
-             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            // string path = @"C:\Games\Тренировка.txt";
+            // StreamWriter sw = new StreamWriter(path, true);
+            //if (System.IO.File.Exists(path))
+            //{
+            //    // Если - да, то сообщаем об этом
+            //    Console.WriteLine("\nФайл OutputTime.txt существует\n");
+            //    Console.ReadKey();
+            //    return
+            //}
+            //else
+            //{
+            //    // Если - нет, тоже сообщаем 
+            //    Console.WriteLine("\nФайл Output.txt отсутствует!\nПриложение автоматически создаст его!\n");
+            //    return
+            //}
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
             if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
             {
+                if (logging)
+                {
+                    Log(update);
+                    logging = false;
+                }
                 var message = update.Message;
                 if (message.Text.ToLower() == "/start")//нижний регистор ToLower
                 {
                     await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник!");
                     return;
                 }
-                if (message.Text.ToLower() == "Привет")
+                if (message.Text == "Привет")
                 {
                     await botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
                     return;
                 }
-                if (message.Text.ToLower() == "/тренировка")
+                if (message.Text.ToLower() == "/add")
                 {
+                    message = update.Message;
                     await botClient.SendTextMessageAsync(message.Chat, "А теперь отправь свою тренировку");
-                    try
-                    {                       
-                        var botUpdatesString = System.IO.File.ReadAllText(fileName);
-                        botUpdates = JsonConvert.DeserializeObject<List<BotUpdate>>(botUpdatesString) ?? botUpdates;
+                    await botClient.SendTextMessageAsync(message.Chat, "Для отмены напиши /back");
+                    if (message.Text.ToLower() != "/back")
+                    { try
+
+                        {
+                            logging = true;
+                            //sw.WriteLine(botUpdatesString);
+                            // System.IO.File.AppendAllText(path, botUpdatesString);
+                            await botClient.SendTextMessageAsync(message.Chat, "Тренировка записана");
+                            return;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error reading or deserializing {ex}");
+                        }
                     }
-                    catch (Exception ex)
+
+                    else
                     {
-                        Console.WriteLine($"Error reading or deserializing {ex}");
+                        await botClient.SendTextMessageAsync(message.Chat, "Запись отменена");
+                        return;
                     }
+                   
                     var receiverOptions = new ReceiverOptions
                     {
                         AllowedUpdates = new UpdateType[]
@@ -59,23 +105,35 @@ namespace VIOrientemBot
                     UpdateType.Message,
                     UpdateType.EditedMessage,
                 }
-                    };
-                    //message = update.Message;
-                    //string workout = message.Text;
-                    //sw.WriteLine(workout);
-
+                    };                  
                 }
-                if(message.Text.ToLower() == "/help")
+                if(message.Text.ToLower() == "/read")      //получения списка тренировак
                 {
-                    await botClient.SendTextMessageAsync(message.Chat, "/тренировка - для записи тренировки");
+                    StreamReader sr = new StreamReader("updates.json");                   
+                    string line;                       
+                    while ((line = sr.ReadLine()) != null)
+                    await botClient.SendTextMessageAsync(message.Chat, line);
+                    return;                    
+                }                               
+                if(message.Text.ToLower() == "/help")     //помощь 
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "/add - для записи тренировки");
                     await botClient.SendTextMessageAsync(message.Chat, "/help - для получения списка команд");
+                    await botClient.SendTextMessageAsync(message.Chat, "/read - для получения списка всех тренировак");
+                    return;
                 }                
                 await botClient.SendTextMessageAsync(message.Chat, "Попробуй другие команды");
             }
         }
-        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+
+        private static void Log(Update update)
         {
-            // Некоторые действия
+            Message? message = update.Message;
+            System.IO.File.AppendAllText(fileName, $"\n{message.Chat.FirstName}: {message.Text}({DateTime.Now}) ");            
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {            
             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
         }
         static void Main(string[] args)
@@ -87,12 +145,8 @@ namespace VIOrientemBot
                     UpdateType.Message,
                     UpdateType.EditedMessage,
                 }
-            };
-
-            bot.StartReceiving(UpdateHandler, ErrorHandler, receiverOptions);            
-            //
+            };           
             Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
-
             var cts = new CancellationTokenSource();
             var cancellationToken = cts.Token;
              receiverOptions = new ReceiverOptions
@@ -127,10 +181,6 @@ namespace VIOrientemBot
                     System.IO.File.WriteAllText(fileName, botUpdatesString);
                 }
             }
-        }
-        private static Task ErrorHandler(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
-        {
-            throw new NotImplementedException();
-        }
+        }       
     }
 }
